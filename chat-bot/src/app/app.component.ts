b148548d593d0179a52945f191497d4f2b9cf82d
montshejohn <mokgokongjohannes.com>
2018-12-21 15:11:13 +0200

    create minal chat bot ui

--- chat-bot/src/app/app.component.ts
+++ chat-bot/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'chat-bot';
+  title = "chat-bot";
+  button = `lets check`;
+  btns = ["User cannot login", "User has probelms with Java", "The system is down"];
 }
