da6b9042990206efe2ae338c1f73f3162fd7e04d
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-12-22 02:11:32 +0200

    created an array that stores the options.

--- chat-bot/src/app/app.component.ts
+++ chat-bot/src/app/app.component.ts
@@ -10,31 +10,37 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   title = "chat-bot";
   messagesAndResponses = [];
+  btns = [];
   constructor(private http: HttpClient) {
     this.getData();
     this.getOptions();
   };
+
   getData(): Observable<HttpResponse<Options[]>> {
     return this.http.get<Options[]>('http://41.86.98.151:8080/tree?name=test', { observe: 'response' });
   };
+
   getOptions() {
     this.getData().subscribe(
       data => {
+        this.btns = data.body.node;
         this.messagesAndResponses.push({ data: data.body.text, style: "speech-bubble" });
       },
       err => console.error(err),
     );
-    return this.messagesAndResponses;
+    return this.btns;
   }
+
   selectedOption(e) {
     this.messagesAndResponses.push({ data: e.option, style: "speech-bubble-response" });
     this.getData().subscribe(
       data => {
+        this.btns = data.body.node;
         this.messagesAndResponses.push({ data: "The api was successful t;aksaf;lkasd;lskf'kla;f;lsk fasdlkfl ;slak  ;kl as;ldkf ;laskf;lksa;dflk s;klf ;skaf;lsk?", style: "speech-bubble" });
       },
       err => console.error(err),
     );
-    return this.messagesAndResponses;
+    return this.btns;
   }
 
 }
