39e5cf761c7cacf02aac0374b234a3e603defaec
montshejohn <mokgokongjohannes.com>
2018-12-26 09:01:53 +0200

    made changes to the bot ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Options } from './app.options'
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { Options } from "./app.options";
+import { Observable } from "rxjs";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -17,43 +17,58 @@ export class AppComponent {
     // this.getFirstOptions();
     // this.getOptions();
     // this.showOptions();
-  };
+  }
   Reset() {
     this.messagesAndResponses = [];
     this.btns = [];
-    this.ShowFirstOptions()
+    this.ShowFirstOptions();
   }
   getFirstOptions(): Observable<HttpResponse<Options>> {
-    return this.http.get<Options>('http://41.86.98.151:8080/tree?name=test', { observe: 'response' });
-  };
+    return this.http.get<Options>("http://41.86.98.151:8080/tree?name=test", {
+      observe: "response"
+    });
+  }
 
   getOptions(nodeId: string): Observable<HttpResponse<Options>> {
-    return this.http.get<Options>('http://41.86.98.151:8080/node?nodeid=' + nodeId, { observe: 'response' });
-  };
+    return this.http.get<Options>(
+      "http://41.86.98.151:8080/node?nodeid=" + nodeId,
+      { observe: "response" }
+    );
+  }
 
   ShowFirstOptions() {
     this.getFirstOptions().subscribe(
       data => {
         this.btns = data.body.node;
-        this.messagesAndResponses.push({ data: data.body.text, style: "speech-bubble" });
+        this.messagesAndResponses.push({
+          data: data.body.text,
+          style: "speech-bubble"
+        });
       },
-      err => console.error(err),
+
+      err => console.error(err)
     );
     return this.btns;
   }
 
   showOptions(selectedOption) {
-    this.messagesAndResponses.push({ data: selectedOption.option, style: "speech-bubble-response" });
+    this.messagesAndResponses.push({
+      data: selectedOption.option,
+      style: "speech-bubble-response"
+    });
     this.getOptions(selectedOption.nodeid).subscribe(
       data => {
         if (data.body.text) {
           this.btns = data.body.node;
-          this.messagesAndResponses.push({ data: data.body.text, style: "speech-bubble" });
+          this.messagesAndResponses.push({
+            data: data.body.text,
+            style: "speech-bubble"
+          });
+          window.scrollTo(0, 10000);
         }
       },
-      err => console.error(err),
+      err => console.error(err)
     );
     return this.btns;
   }
-
 }
