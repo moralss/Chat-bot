0babdb3c6b17eaaf65ec23fe0bc5a74f55f91b59
montshejohn <mokgokongjohannes.com>
2019-01-03 21:40:54 +0200

    restore files

---
+++ chat-bot/src/app/speak-to-agent/speak-to-agent.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SpeakToAgentComponent } from './speak-to-agent.component';
+
+describe('SpeakToAgentComponent', () => {
+  let component: SpeakToAgentComponent;
+  let fixture: ComponentFixture<SpeakToAgentComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SpeakToAgentComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SpeakToAgentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ chat-bot/src/app/speak-to-agent/speak-to-agent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-speak-to-agent',
+  templateUrl: './speak-to-agent.component.html',
+  styleUrls: ['./speak-to-agent.component.css']
+})
+export class SpeakToAgentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
