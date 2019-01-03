4fa7036f6bc00123f369c256f0680b2aed61fd57
SKMsibi <sabelokabelomsibiskm@gmail.com>
2019-01-03 12:49:09 +0200

    edited the routing feature.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SpeakToAgentComponent } from './speak-to-agent/speak-to-agent.component'
+import { AppComponent } from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'speak-to-agent', component: SpeakToAgentComponent },
+  { path: 'home', component: AppComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
