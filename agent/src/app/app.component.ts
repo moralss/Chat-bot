c103ed03e620596b93de8913449bd49822774475
SKMsibi <sabelokabelomsibiskm@gmail.com>
2019-01-04 19:44:08 +0200

    added a provider for the shared service.

--- agent/src/app/app.component.ts
+++ agent/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from "@angular/core";
+import { DataService } from './data.service';
+import { ViewAllSessionIdsComponent } from './view-all-session-ids/view-all-session-ids.component';
+import { UserOperatorChatComponent } from './user-operator-chat/user-operator-chat.component';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
+  providers: [DataService],
 })
 export class AppComponent {
-  
+
 }
