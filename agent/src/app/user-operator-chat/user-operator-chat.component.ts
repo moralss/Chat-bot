371e62fc19eb635edb9da9bca3895a64f79fec0a
SKMsibi <sabelokabelomsibiskm@gmail.com>
2019-01-04 13:54:02 +0200

    created the component ts file for a new component.

---
+++ agent/src/app/user-operator-chat/user-operator-chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-operator-chat',
+  templateUrl: './user-operator-chat.component.html',
+  styleUrls: ['./user-operator-chat.component.css']
+})
+export class UserOperatorChatComponent implements OnInit {
+
+  constructor() { }
+
+  
+  ngOnInit() {
+  }
+}
