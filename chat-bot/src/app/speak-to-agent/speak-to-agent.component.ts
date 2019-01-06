b23cef4a2113f238c629dcf39695ce569f21afec
SKMsibi <sabelokabelomsibiskm@gmail.com>
2019-01-06 13:01:16 +0200

    refactored code.

--- chat-bot/src/app/speak-to-agent/speak-to-agent.component.ts
+++ chat-bot/src/app/speak-to-agent/speak-to-agent.component.ts
@@ -9,21 +9,34 @@ import { HttpClient, HttpResponse } from "@angular/common/http";
 export class SpeakToAgentComponent implements OnInit {
   sessionId = null;
   userBotChats = []
-  userToAgentMessages = [];
+  messages = [];
   constructor(private data: DataService, private http: HttpClient) {
-    setInterval(() => {
-      this.getSessionIdMessages(this.sessionId).subscribe((data: any) => {
-        console.log("messages", data)
-        // this.messagesAndResponses.push({
-        //   data: data.message,
-        //   style: "speech-bubble",
-        //   image: selectedOption.nodeimage
-        //     ? "data:image/jpeg;base64," + selectedOption.nodeimage
-        //     : null
-        // });
-      })
-
+    setTimeout(() => {
+      this.getMessages(true)
     }, 1000);
+    setInterval(() => {
+      this.getMessages(false)
+    }, 4000);
+  }
+
+  getMessages(onLoad: boolean) {
+    this.getSessionIdMessages(this.sessionId).subscribe((data: any) => {
+      console.log('data :', data);
+      data.message.forEach(element => {
+        if (element.type === "User") {
+          element.style = "speech-bubble-response";
+        } else {
+          element.style = "speech-bubble";
+        }
+      });
+      this.messages = data.message;
+    })
+  }
+  Reset(status: boolean) {
+    this.messages = [];
+    if (status) {
+      window.location.href = "/";
+    }
   }
   getSessionIdMessages(sessionId: string) {
     return this.http.get("http://41.86.98.151:8080/getChat?sessionId=" + sessionId);
@@ -37,7 +50,7 @@ export class SpeakToAgentComponent implements OnInit {
       this.userBotChats = messages
     });
     this.data.userAgentMessages.subscribe((messages: any) => {
-      this.userToAgentMessages = messages
+      this.messages = messages
     })
   }
 
