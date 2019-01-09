df15f2ab80864050f9d18c85eab4bc1a53155e69
SKMsibi <sabelokabelomsibiskm@gmail.com>
2019-01-09 14:25:52 +0200

    added a safe pipe module.

--- chat-bot/src/app/app.module.ts
+++ chat-bot/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { SpeakToAgentComponent } from "./speak-to-agent/speak-to-agent.component";
 import { SpeakToBotComponent } from './speak-to-bot/speak-to-bot.component';
+import { SafePipe } from './safe.pipe';
 
 @NgModule({
-  declarations: [AppComponent, SpeakToAgentComponent, SpeakToBotComponent],
+  declarations: [AppComponent, SpeakToAgentComponent, SpeakToBotComponent, SafePipe],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
