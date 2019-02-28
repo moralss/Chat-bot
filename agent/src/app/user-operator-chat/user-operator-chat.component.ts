0bf2c547bc13fc05ef9e12be1d715ac8622f7638
skmsibi <sabelokabelomsibiksm@gmail.com>
2019-02-28 11:16:01 +0200

    changed auto scroll from window to div, formated code.

--- agent/src/app/user-operator-chat/user-operator-chat.component.ts
+++ agent/src/app/user-operator-chat/user-operator-chat.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { DataService } from "../data.service";
 import { HttpClient, HttpResponse } from "@angular/common/http";
 import { Observable } from "rxjs";
 @Component({
-  selector: 'app-user-operator-chat',
-  templateUrl: './user-operator-chat.component.html',
-  styleUrls: ['./user-operator-chat.component.css']
+  selector: "app-user-operator-chat",
+  templateUrl: "./user-operator-chat.component.html",
+  styleUrls: ["./user-operator-chat.component.css"]
 })
 export class UserOperatorChatComponent implements OnInit {
   messagesAndResponses = [];
-  userSessionId = '';
-  text = '';
+  userSessionId = "";
+  text = "";
   number = 1;
   constructor(private http: HttpClient, private data: DataService) {
     setTimeout(() => {
@@ -23,6 +23,7 @@ export class UserOperatorChatComponent implements OnInit {
         this.getData();
       }
     }, 3000);
+    
   }
   sendMessage(event) {
     if (event) {
@@ -34,7 +35,11 @@ export class UserOperatorChatComponent implements OnInit {
           orderId: this.messagesAndResponses.length + 1
         };
         this.messagesAndResponses.push(message);
-        this.sendMessageToApi({ data: value, type: "agent", orderId: this.messagesAndResponses.indexOf(message) + 1 }).subscribe();
+        this.sendMessageToApi({
+          data: value,
+          type: "agent",
+          orderId: this.messagesAndResponses.indexOf(message) + 1
+        }).subscribe();
         event.path[0].value = "";
       } else {
         this.text = event.path[0].value;
@@ -47,7 +52,11 @@ export class UserOperatorChatComponent implements OnInit {
           orderId: this.messagesAndResponses.length + 1
         };
         this.messagesAndResponses.push(message);
-        this.sendMessageToApi({ data: this.text, type: "agent", orderId: this.messagesAndResponses.indexOf(message) + 1 }).subscribe();
+        this.sendMessageToApi({
+          data: this.text,
+          type: "agent",
+          orderId: this.messagesAndResponses.indexOf(message) + 1
+        }).subscribe();
       }
     }
     this.getNewMessages(this.userSessionId);
@@ -75,18 +84,32 @@ export class UserOperatorChatComponent implements OnInit {
         }
         element.orderId = +element.orderId;
       });
-
+      var elem = document.getElementById("chat");
+      elem.scrollTop = elem.scrollHeight;
       this.messagesAndResponses = message.messages;
-    })
+    });
   }
   sendMessageToApi(message: any) {
-    return this.http.get("http://41.86.98.151:8080/addMessage?type=" + message.type + "&message=" + message.data + "&sessionId=" + this.userSessionId + "&orderId=" + `${message.orderId}`);
+    return this.http.get(
+      "http://41.86.98.151:8080/addMessage?type=" +
+        message.type +
+        "&message=" +
+        message.data +
+        "&sessionId=" +
+        this.userSessionId +
+        "&orderId=" +
+        `${message.orderId}`
+    );
   }
   closeSession() {
-    return this.http.get("http://41.86.98.151:8080/removeSession?sessionId=" + this.userSessionId);
+    return this.http.get(
+      "http://41.86.98.151:8080/removeSession?sessionId=" + this.userSessionId
+    );
   }
   getSessionIdMessages(sessionId: string) {
-    return this.http.get("http://41.86.98.151:8080/getChat?sessionId=" + sessionId);
+    return this.http.get(
+      "http://41.86.98.151:8080/getChat?sessionId=" + sessionId
+    );
   }
   getNewMessages(sessionId: string) {
     this.getSessionIdMessages(sessionId).subscribe((data: any) => {
@@ -110,25 +133,26 @@ export class UserOperatorChatComponent implements OnInit {
         }
         element.orderId = +element.orderId;
       });
-      window.scrollTo({
-        top: document.documentElement.scrollHeight,
-        left: document.documentElement.scrollHeight,
-        behavior: "smooth"
-      });
       data.message.sort((a, b) => {
         if (a.orderId < b.orderId) return -1;
         if (a.orderId > b.orderId) return 1;
         return 0;
-      })
-      this.messagesAndResponses = data.message;
-      if (data.message.length > 0) {
-        this.data.changeMessage({ sessionId: this.userSessionId, messages: data.message });
+      });
+      if (data.message.length > this.messagesAndResponses.length) {
+        this.messagesAndResponses = data.message;
+        var elem = document.getElementById("chat");
+        elem.scrollTop = elem.scrollHeight;
       }
-    })
+      if (data.message.length > 0) {
+        this.data.changeMessage({
+          sessionId: this.userSessionId,
+          messages: data.message
+        });
+      }
+    });
   }
   endChat() {
     this.closeSession().subscribe();
   }
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
