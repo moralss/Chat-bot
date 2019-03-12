e264ca89d53a99f239e164c1a3bfc13d3918bba0
skmsibi <sabelokabelomsibiksm@gmail.com>
2019-03-12 12:36:33 +0200

    refactord code. fixed bugs.

--- agent/src/app/view-all-session-ids/view-all-session-ids.component.ts
+++ agent/src/app/view-all-session-ids/view-all-session-ids.component.ts
@@ -1,26 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { HttpClient, HttpResponse } from "@angular/common/http";
-import { Sessions } from "./view-all-session-ids.sessions"
+import { Sessions } from "./view-all-session-ids.sessions";
 import { Observable } from "rxjs";
 import { DataService } from "../data.service";
 import { environment } from "../../environments/environment";
 
-
-
 @Component({
-  selector: 'app-view-all-session-ids',
-  templateUrl: './view-all-session-ids.component.html',
-  styleUrls: ['./view-all-session-ids.component.css']
+  selector: "app-view-all-session-ids",
+  templateUrl: "./view-all-session-ids.component.html",
+  styleUrls: ["./view-all-session-ids.component.css"]
 })
-
 export class ViewAllSessionIdsComponent implements OnInit {
   apiAddress = environment.apiAddress;
   title = "Chat bot";
   allAvailableSessions = [];
   sessionIdMessages = [];
-  message = []
+  message = [];
   constructor(private http: HttpClient, private data: DataService) {
     this.ShowSessionIds();
+    setInterval(() => {
+      this.ShowSessionIds();
+    }, 2000);
   }
   getSessionIds(): Observable<HttpResponse<Sessions>> {
     return this.http.get<Sessions>(this.apiAddress + "getAgentSessions", {
@@ -29,18 +29,17 @@ export class ViewAllSessionIdsComponent implements OnInit {
   }
 
   ShowSessionIds() {
-    this.getSessionIds().subscribe(
-      (data: any) => {
-        data.body.map(element => {
-          this.allAvailableSessions.push(element);
-        });
-        this.allAvailableSessions = this.allAvailableSessions.sort(function (a, b) {
-          a = new Date(a.split(" ")[1] + " " + a.split(" ")[2]);
-          b = new Date(b.split(" ")[1] + " " + b.split(" ")[2]);
-          return a > b ? 1 : a < b ? -1 : 0;
-        });
-      }
-    )
+    this.getSessionIds().subscribe((data: any) => {
+      this.allAvailableSessions = data.body;
+      this.allAvailableSessions = this.allAvailableSessions.sort(function(
+        a,
+        b
+      ) {
+        a = new Date(a.split(" ")[1] + " " + a.split(" ")[2]);
+        b = new Date(b.split(" ")[1] + " " + b.split(" ")[2]);
+        return a > b ? 1 : a < b ? -1 : 0;
+      });
+    });
   }
 
   getSessionIdMessages(sessionId: string) {
@@ -48,11 +47,12 @@ export class ViewAllSessionIdsComponent implements OnInit {
   }
   selectedSessionId(sessionId: string) {
     this.getSessionIdMessages(sessionId).subscribe((data: any) => {
-      this.sessionIdMessages.push({ sessionId: sessionId, messages: data.message });
+      this.sessionIdMessages.push({
+        sessionId: sessionId,
+        messages: data.message
+      });
       this.data.changeMessage({ sessionId: sessionId, messages: data.message });
-    })
+    });
   }
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
